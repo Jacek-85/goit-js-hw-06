76f688e4ef3b981e71bf951868c2ed70a867b56c
Jacek_Blaszczyk <111233513+Jacek-85@users.noreply.github.com>
2023-03-11 11:59:42 +0100

    version 1

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", (event) => {
+  if (event.target.value.length == input.getAttribute("data-length")) {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.add("invalid");
+    input.classList.remove("valid");
+  }
+});
